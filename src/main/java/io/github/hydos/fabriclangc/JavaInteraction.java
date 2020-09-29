86a6e99750d985f45b27a6a0d6bbebd6bf901a6c
hydos <haydenv06@gmail.com>
2020-09-29 21:00:33 +1000

    add C code

---
+++ src/main/java/io/github/hydos/fabriclangc/JavaInteraction.java
@@ -0,0 +1,9 @@
+package io.github.hydos.fabriclangc;
+
+public class JavaInteraction {
+
+    public static void printHello(){
+        System.out.println("The Native Code Is Working Somehow");
+    }
+
+}
--- src/main/java/io/github/hydos/fabriclangc/TestClass.java
+++
@@ -1,9 +0,0 @@
-package io.github.hydos.fabriclangc;
-
-public class TestClass {
-
-    public static void printHello(){
-        System.out.println("Hello From Java called by C!");
-    }
-
-}
