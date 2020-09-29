144eaebca3441dedb1844ec51f2018331211ccc1
hydos <haydenv06@gmail.com>
2020-09-29 20:50:16 +1000

    i dont even know

--- src/main/java/io/github/hydos/fabriclangc/CBridge.java
+++ src/main/java/io/github/hydos/fabriclangc/CBridge.java
@@ -2,14 +2,19 @@ package io.github.hydos.fabriclangc;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Bootstrap implements ModInitializer {
+public class CBridge implements ModInitializer{
 
+    static {
+        System.loadLibrary("FabricLangaugeC");
+    }
+
+    static native void parseJniEnv();
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
-
+        parseJniEnv();
     }
 }
---
+++ src/main/java/io/github/hydos/fabriclangc/TestClass.java
@@ -0,0 +1,9 @@
+package io.github.hydos.fabriclangc;
+
+public class TestClass {
+
+    public static void printHello(){
+        System.out.println("Hello From Java called by C!");
+    }
+
+}
