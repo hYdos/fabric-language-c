ec0c3769b227dd7989456cd497734d6d406adec5
hydos <haydenv06@gmail.com>
2020-10-04 08:50:53 +1000

    bababooey

--- src/main/java/io/github/hydos/fabriclangc/NativeModInitializer.java
+++ src/main/java/io/github/hydos/fabriclangc/NativeModInitializer.java
@@ -5,6 +5,7 @@ import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,12 +15,21 @@ public class NativeModInitializer implements ModInitializer {
     public static final File TEMP_DIR = new File(FabricLoader.getInstance().getGameDir().toAbsolutePath() + "/temp");
 
     public NativeModInitializer(String libName, String modid) {
+        Path extractedNativeLoc = new File(TEMP_DIR, libName).toPath();
+        Path b = FabricLoader.getInstance().getModContainer(modid).get().getPath(libName);
         try {
-            Path extractedNativeLoc = new File(TEMP_DIR, libName).toPath();
-            Path b = FabricLoader.getInstance().getModContainer(modid).get().getPath(libName);
             Files.createDirectories(Paths.get(String.valueOf(TEMP_DIR)));
             Files.copy(b, extractedNativeLoc);
             System.load(extractedNativeLoc.toAbsolutePath().toString());
+        } catch (FileAlreadyExistsException e){
+            try {
+                Files.delete(extractedNativeLoc);
+                Files.createDirectories(Paths.get(String.valueOf(TEMP_DIR)));
+                Files.copy(b, extractedNativeLoc);
+                System.load(extractedNativeLoc.toAbsolutePath().toString());
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
         } catch (UnsatisfiedLinkError | IOException e) {
             System.err.println("Native code library failed to load.");
             e.printStackTrace();
